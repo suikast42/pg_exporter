38b9df0ce334b598237c1f4f9c30b3f497deb653
Feng Ruohang <rh@vonng.com>
2026-01-15 23:02:52 +0800

    bump version to 1.1.2

--- exporter/global.go
+++ exporter/global.go
@@ -9,7 +9,7 @@ import (
 /* ================ Parameters ================ */
 
 // Version is read by make build procedure
-var Version = "1.1.1"
+var Version = "1.1.2"
 
 // Build information. Populated at build-time.
 var (
